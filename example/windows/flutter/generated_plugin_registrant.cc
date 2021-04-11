af197e21b4a0baf838d8b647ede436837e000b99
Sunbreak <sunbreak.wang@gmail.com>
2021-04-11 09:36:33 +0800

    Initial commit
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <cronet_flutter/cronet_flutter_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  CronetFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("CronetFlutterPlugin"));
+}
