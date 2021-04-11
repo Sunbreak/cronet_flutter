6e112b8c6276bf51d2ae7b33c9e1d5f5a704ecb7
Sunbreak <sunbreak.wang@gmail.com>
2021-04-11 10:32:12 +0800

    Add Windows resource
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- lib/cronet_flutter.dart
+++ lib/cronet_flutter.dart
@@ -1,14 +1,33 @@
+import 'dart:ffi';
+import 'dart:io';
 
-import 'dart:async';
+import 'package:ffi/ffi.dart';
 
-import 'package:flutter/services.dart';
+import 'src/cronet.dart';
+
+export 'src/cronet.dart';
+
+final DynamicLibrary Function() loadLibrary = () {
+  if (Platform.isWindows) {
+    return DynamicLibrary.open('cronet.86.0.4240.198.dll');
+  }
+  throw UnimplementedError();
+};
+
+final _croNet = CroNet(loadLibrary());
 
 class CronetFlutter {
-  static const MethodChannel _channel =
-      const MethodChannel('cronet_flutter');
+  static String getVersionString() {
+    var enginePtr = _croNet.Cronet_Engine_Create();
+    var engineParamsPtr = _croNet.Cronet_EngineParams_Create();
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+    try {
+      _croNet.Cronet_Engine_StartWithParams(enginePtr, engineParamsPtr);
+
+      return _croNet.Cronet_Engine_GetVersionString(enginePtr).cast<Utf8>().toDartString();
+    } finally {
+      _croNet.Cronet_EngineParams_Destroy(engineParamsPtr);
+      _croNet.Cronet_Engine_Destroy(enginePtr);
+    }
   }
 }
