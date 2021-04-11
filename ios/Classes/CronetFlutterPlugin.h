3492bc596ece1be5939c76567607d4639ab881a5
Sunbreak <sunbreak.wang@gmail.com>
2021-04-12 00:55:02 +0800

    Add iOS project
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

---
+++ ios/Classes/CronetFlutterPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface CronetFlutterPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/CronetFlutterPlugin.m
@@ -0,0 +1,15 @@
+#import "CronetFlutterPlugin.h"
+#if __has_include(<cronet_flutter/cronet_flutter-Swift.h>)
+#import <cronet_flutter/cronet_flutter-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "cronet_flutter-Swift.h"
+#endif
+
+@implementation CronetFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftCronetFlutterPlugin registerWithRegistrar:registrar];
+}
+@end
