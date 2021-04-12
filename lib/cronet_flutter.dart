9e94f26309b5e43465ba7ccd4daf0b414ba389a7
Sunbreak <sunbreak.wang@gmail.com>
2021-04-12 15:52:05 +0800

    Add Android resource
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- lib/cronet_flutter.dart
+++ lib/cronet_flutter.dart
@@ -14,6 +14,8 @@ final DynamicLibrary Function() loadLibrary = () {
     return DynamicLibrary.open('libcronet.86.0.4240.198.dylib');
   } else if (Platform.isIOS) {
     return DynamicLibrary.process();
+  } else if (Platform.isAndroid) {
+    return DynamicLibrary.open('libcronet.86.0.4240.198.so');
   }
   throw UnimplementedError();
 };
@@ -23,14 +25,9 @@ final _croNet = CroNet(loadLibrary());
 class CronetFlutter {
   static String getVersionString() {
     var enginePtr = _croNet.Cronet_Engine_Create();
-    var engineParamsPtr = _croNet.Cronet_EngineParams_Create();
-
     try {
-      _croNet.Cronet_Engine_StartWithParams(enginePtr, engineParamsPtr);
-
       return _croNet.Cronet_Engine_GetVersionString(enginePtr).cast<Utf8>().toDartString();
     } finally {
-      _croNet.Cronet_EngineParams_Destroy(engineParamsPtr);
       _croNet.Cronet_Engine_Destroy(enginePtr);
     }
   }
